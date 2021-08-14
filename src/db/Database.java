dac703df57bcc794bfa1a68c5d20c699d83d0d37
guilhermeaw <guilherme.w@universo.univates.br>
2021-08-13 22:23:26 -0300

    Criada classe para configurar conexão com o banco

---
+++ src/db/Database.java
@@ -0,0 +1,49 @@
+package db;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+public class Database {
+  private static Database instance;
+
+  public static Database getInstance() {
+    if (instance == null) {
+      instance = new Database();
+    }
+
+    return instance;
+  }
+
+  private SessionFactory sessionFactory;
+
+  private Database() {
+    try {
+      setupHibernate();
+    } catch (Exception error) {
+
+    }
+  }
+
+  public Session getSession() {
+    Session session = sessionFactory.openSession();
+    return session;
+  }
+
+  protected void setupHibernate() throws Exception {
+    // A SessionFactory is set up once for an application!
+    final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+        .configure() // configures settings from hibernate.cfg.xml
+        .build();
+    try {
+      sessionFactory = new MetadataSources( registry ).buildMetadata().buildSessionFactory();
+    }
+    catch (Exception e) {
+      // The registry would be destroyed by the SessionFactory, but we had trouble building the SessionFactory
+      // so destroy it manually.
+      StandardServiceRegistryBuilder.destroy( registry );
+    }
+  }
+}

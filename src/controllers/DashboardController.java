c28870dfb7fae4254a854460ef843ede06916c52
guilhermeaw <guilherme.w@universo.univates.br>
2021-08-20 22:07:50 -0300

    Adicionadas abas de cidade e estado

--- src/controllers/DashboardController.java
+++ src/controllers/DashboardController.java
@@ -1,16 +1,46 @@
 package controllers;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
 import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 import services.SceneChangeService;
+import utils.ApplicationUtilities;
 
-public class DashboardController {
+public class DashboardController implements Initializable {
     @FXML
     private Button buttonLogout;
 
     @FXML
+    private StackPane dashboardStackPane;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+      loadDefaultPane();
+    }
+    
+    @FXML
     void handleLogout(ActionEvent event) {
       new SceneChangeService().changeSceneTo("/views/login.fxml");
+    }
+
+    private void loadDefaultPane() {
+      loadPane("/views/components/panes/citiesStatesTabbedPane.fxml");
+    }
+
+    private void loadPane(String paneSrc) {
+      try {
+          Parent pane = FXMLLoader.load(getClass().getResource(paneSrc));
+
+          dashboardStackPane.getChildren().setAll(pane);
+      } catch (Exception e) {
+          ApplicationUtilities.getInstance().handleException(e);
+      }
     }
 }

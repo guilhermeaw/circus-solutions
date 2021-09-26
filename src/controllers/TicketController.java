1ec5b5958021c1a7d0982b497408b3f8dab9a63a
guilhermeaw <guilherme.w@universo.univates.br>
2021-09-25 23:33:19 -0300

    Adiciona tela de ingressos

---
+++ src/controllers/TicketController.java
@@ -0,0 +1,33 @@
+package controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class TicketController {
+    @FXML
+    private TextField fieldValue;
+
+    @FXML
+    private TextField fieldLastUpdate;
+
+    @FXML
+    private TextField fieldAuthor;
+
+    @FXML
+    private Button buttonCancel;
+
+    @FXML
+    private Button buttonConfirm;
+
+    @FXML
+    void handleUpdateTicketConfig(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleCancel(ActionEvent event) {
+
+    }
+}
